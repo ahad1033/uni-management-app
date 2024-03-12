cdb3e7bd1971cab122acf9903835c372d5340545
ahad1033 <a.ahad2232@gmail.com>
2024-03-13 03:09:35 +0600

    change routing and left nav bar

---
+++ src/pages/admin/CreateFaculty.tsx
@@ -0,0 +1,9 @@
+const CreateFaculty = () => {
+  return (
+    <div>
+      <h1>this is faculty component</h1>
+    </div>
+  );
+};
+
+export default CreateFaculty;
--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -6,6 +6,8 @@ import About from "../pages/About";
 import Contact from "../pages/Contact";
 import AdminDashboard from "../pages/admin/AdminDashboard";
 import CreateStudent from "../pages/admin/CreateStudent";
+import CreateAdmin from "../pages/admin/CreateAdmin";
+import CreateFaculty from "../pages/admin/CreateFaculty";
 
 const router = createBrowserRouter([
   {
@@ -27,16 +29,20 @@ const router = createBrowserRouter([
     element: <App />,
     children: [
       {
-        index: true,
-        element: <AdminDashboard />,
-      },
-      {
         path: "dashboard",
         element: <AdminDashboard />,
       },
       {
         path: "create-student",
         element: <CreateStudent />,
+      },
+      {
+        path: "create-admin",
+        element: <CreateAdmin />,
+      },
+      {
+        path: "create-faculty",
+        element: <CreateFaculty />,
       },
     ],
   },
