89fd3b2e127357274cf5121f60fc7c79990e348b
ahad1033 <a.ahad2232@gmail.com>
2024-03-12 21:48:06 +0600

    added pages and modify routing

--- src/components/layout/MainLayout.tsx
+++ src/components/layout/MainLayout.tsx
@@ -1,4 +1,5 @@
 import { Layout, Menu, MenuProps } from "antd";
+import { Outlet } from "react-router-dom";
 
 const { Content, Footer, Header, Sider } = Layout;
 
@@ -67,7 +68,7 @@ const MainLayout = () => {
               minHeight: 360,
             }}
           >
-            <h1>the main content should go here.....</h1>
+            <Outlet />
           </div>
         </Content>
         <Footer style={{ textAlign: "center" }}>
